96541abacfc2294bfa4c0f0dee6c1eff851c9dc7
Rangel Stoilov <rangelstoilov@gmail.com>
2021-03-09 00:44:34 +0200

    return json form api

--- src/common/api/TypiCodeApi.js
+++ src/common/api/TypiCodeApi.js
@@ -1,18 +1,18 @@
 class TypiCodeApi {
 	constructor() {
-		this.SERVER_ENDPOINT = "https://jsonplaceholder.typicode.com/"
+		this.SERVER_ENDPOINT = "https://jsonplaceholder.typicode.com"
 	}
 
 	fetchPosts = () => {
 		return fetch(`${this.SERVER_ENDPOINT}/posts`, {
 			method: "GET",
-		})
+		}).then((res) => res.json())
 	}
 
 	fetchUsers = () => {
 		return fetch(`${this.SERVER_ENDPOINT}/users`, {
 			method: "GET",
-		})
+		}).then((res) => res.json())
 	}
 }
 
