a19be0deaaf059cf5e5b97d3439a602d19e7d23b
Rangel Stoilov <rangelstoilov@gmail.com>
2021-03-09 11:42:25 +0200

    connect users to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
-import metamaskReducer from "../features/metamask/metamaskSlice"
-import postsReducer from "../features/posts/postsSlice"
+import metamaskReducer from "features/metamask/metamaskSlice"
+import postsReducer from "features/posts/postsSlice"
+import usersReducer from "features/usersPage/usersSlice"
 
 export default configureStore({
 	reducer: {
 		metamask: metamaskReducer,
 		posts: postsReducer,
+		users: usersReducer,
 	},
 })
